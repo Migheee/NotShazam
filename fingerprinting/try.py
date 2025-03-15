c3062b53c77fc4028f43aba136fc13214ff03ac6
MicheleCastenedoli <mighecaste@gmail.com>
2025-03-15 11:12:24 +0100

    Add execution time logging for key functions in spectrogram and fingerprinting processing

--- fingerprinting/try.py
+++ fingerprinting/try.py
@@ -1,17 +1,13 @@
-'''
-Author: Michele Castenedoli
-Creation Date: 04/03/2025
-Last Update: 15/03/2025
-Generates the spectrogram of a song, finds the peaks in the spectrogram
-'''
-
 import os
 import librosa
 import librosa.display 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import maximum_filter
+import time  # Importiamo il modulo per misurare il tempo
 
+# Variabile globale per abilitare o disabilitare i log dei tempi di esecuzione
+ENABLE_LOG = True
 
 FRAME_SIZE = 2048
 HOP_SIZE = 512
@@ -19,32 +15,51 @@ NEIGHBORHOOD_SIZE = 20
 TIME_INTERVAL = 22
 FREQUENCY_INTERVAL = 12
 
+def log_time(func):
+    '''
+    Decoratore per misurare il tempo di esecuzione di una funzione
+    :param func: la funzione da misurare
+    :return: la funzione con il log del tempo
+    '''
+    def wrapper(*args, **kwargs):
+        if ENABLE_LOG:
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            print(f"Tempo di esecuzione di {func.__name__}: {(end_time - start_time) * 1000:.2f} ms")
+        else:
+            result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+
+@log_time
 def get_spectrogram(file_path):
     '''
     This function generates the spectrogram of a song
     :param file_path: path to the song
     :return: the spectrogram and the sampling rate of the song
     '''
-
     # Load song
     song, sr = librosa.load(file_path)
 
     # Execute the STFT
     S = librosa.stft(song, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
 
-    #DB conversion of the amplitude (since the human ear perceives the loudness of a sound on a logarithmic scale)
-    Y_scale = np.abs(S) ** 2  #Squared magnitude of the STFT (to get the power spectogram)
+    # DB conversion of the amplitude (since the human ear perceives the loudness of a sound on a logarithmic scale)
+    Y_scale = np.abs(S) ** 2  # Squared magnitude of the STFT (to get the power spectrogram)
     Y_log = librosa.power_to_db(Y_scale)
 
     return Y_log, sr
 
+
+@log_time
 def plot_spectogram(spectogram, sr):
     '''
     This function plots the spectrogram of a song
     :param spectogram: the spectrogram of the song
     :param sr: the sampling rate of the song
     '''
-
     plt.figure(figsize=(12, 8))
     librosa.display.specshow(spectogram, sr=sr, x_axis='time', y_axis='log')
     plt.colorbar(format='%+2.0f dB')
@@ -52,13 +67,13 @@ def plot_spectogram(spectogram, sr):
     plt.show()
 
 
+@log_time
 def get_peaks(spectrogram):
     '''
     This function finds the peaks in the spectrogram
     :param spectrogram: the spectrogram of the song
     :return: the coordinates of the peaks
     '''
-
     # Find the local maxima in the spectrogram, using a neighborhood of a given size (20x20)
     filtered_spectrogram = maximum_filter(spectrogram, size=NEIGHBORHOOD_SIZE)
     # Find the peaks by comparing the filtered spectrogram with the original one, by element-wise comparison and creating a boolean matrix
@@ -68,37 +83,37 @@ def get_peaks(spectrogram):
     return peak_coords
 
 
+@log_time
 def get_anchor_point(peaks):
     '''
     This function finds the anchor points in the spectrogram
     :param peaks: the coordinates of the peaks
     :return: the anchor points
     '''
-
     anchor_points = []
     for i in range(len(peaks)):
-        for j in range(i+1, len(peaks)): # From i+1 to avoid duplicates abd negative time intervals
+        for j in range(i + 1, len(peaks)):  # From i+1 to avoid duplicates and negative time intervals
             if abs(peaks[i][0] - peaks[j][0]) < TIME_INTERVAL and abs(peaks[i][1] - peaks[j][1]) < FREQUENCY_INTERVAL:
                 anchor_points.append((peaks[i], peaks[j]))
     return anchor_points
 
 
+@log_time
 def get_fingerprint(anchor_points):
     '''
     This function generates the fingerprint of a song
     :param anchor_points: the anchor points
     :return: the fingerprint of the song
     '''
-
     fingerprint = []
     for (f1, t1), (f2, t2) in anchor_points:
         time_diff = t2 - t1
         frequency_diff = f2 - f1
         fingerprint.append((time_diff, frequency_diff))
-    
+
     return fingerprint
 
-    
+
 # Load the song and generate the spectrogram
 spectrogram, sr = get_spectrogram("songs\\Soulmate.wav")
 # Find the peaks in the spectrogram
@@ -108,6 +123,4 @@ anchor_points = get_anchor_point(peaks)
 
 # Generate the fingerprint of the song
 fingerprint = get_fingerprint(anchor_points)
-
-
-
+print(fingerprint)
