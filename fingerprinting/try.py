bc83575293816657c3ad17e9a60eed8899d76b37
MicheleCastenedoli <mighecaste@gmail.com>
2025-03-15 10:45:02 +0100

    Refactoring Code. Function to find peaks

--- fingerprinting/try.py
+++ fingerprinting/try.py
@@ -1,7 +1,8 @@
 '''
 Author: Michele Castenedoli
-Date: 04/03/2025
-Generates the spectrogram of a song
+Creation Date: 04/03/2025
+Last Update: 15/03/2025
+Generates the spectrogram of a song, finds the peaks in the spectrogram
 '''
 
 import os
@@ -9,12 +10,20 @@ import librosa
 import librosa.display 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage import maximum_filter
 
 
 FRAME_SIZE = 2048
 HOP_SIZE = 512
+NEIGHBORHOOD_SIZE = 20
 
 def get_spectrogram(file_path):
+    '''
+    This function generates the spectrogram of a song
+    :param file_path: path to the song
+    :return: the spectrogram and the sampling rate of the song
+    '''
+
     # Load song
     song, sr = librosa.load(file_path)
 
@@ -25,20 +34,40 @@ def get_spectrogram(file_path):
     Y_scale = np.abs(S) ** 2  #Squared magnitude of the STFT (to get the power spectogram)
     Y_log = librosa.power_to_db(Y_scale)
 
+    return Y_log, sr
 
-    # Show the spectrogram
-    plt.figure(figsize=(25, 10))
-    librosa.display.specshow(
-        Y_log,
-        sr=sr,
-        hop_length=HOP_SIZE,
-        x_axis='time',  
-        y_axis='log'
-    )
-    plt.colorbar(format="%+2.f dB")
-    plt.title("Spettrogramma STFT")
-    plt.xlabel("Tempo (s)")
-    plt.ylabel("Frequenza (Hz)")
-    plt.show()  # Show the plot
+def plot_spectogram(spectogram, sr):
+    '''
+    This function plots the spectrogram of a song
+    :param spectogram: the spectrogram of the song
+    :param sr: the sampling rate of the song
+    '''
 
-get_spectrogram("songs\\Soulmate.wav")
+    plt.figure(figsize=(12, 8))
+    librosa.display.specshow(spectogram, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    plt.show()
+
+
+def get_peaks(spectrogram):
+    '''
+    This function finds the peaks in the spectrogram
+    :param spectrogram: the spectrogram of the song
+    :return: the coordinates of the peaks
+    '''
+
+    # Find the local maxima in the spectrogram, using a neighborhood of a given size (20x20)
+    filtered_spectrogram = maximum_filter(spectrogram, size=NEIGHBORHOOD_SIZE)
+    # Find the peaks by comparing the filtered spectrogram with the original one, by element-wise comparison and creating a boolean matrix
+    peaks = (spectrogram == filtered_spectrogram)
+    # Get the coordinates of the peaks, by finding the indices of the True values in the boolean matrix
+    peak_coords = np.argwhere(peaks)
+    return peak_coords
+
+
+
+spectrogram, sr = get_spectrogram("songs\\Soulmate.wav")
+plot_spectogram(spectrogram, sr)
+
+
